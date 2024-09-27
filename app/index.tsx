be9ca46ea2929490bbea8cab0c7f2df861d3fc7a
Esteban Munoz <esteban.munozal@gmail.com>
2024-09-26 22:21:15 -0600

    Build login page

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,160 @@
-import { Text, View } from "react-native";
+import {Image, StyleSheet, Text, TouchableOpacity, View} from "react-native";
+import {Colors} from "@/constants/Colors";
+import {useSafeAreaInsets} from "react-native-safe-area-context";
+import {ModalType} from "@/types/enums";
+import * as WebBrowser from 'expo-web-browser';
+import {useActionSheet} from "@expo/react-native-action-sheet";
+import {BottomSheetBackdrop, BottomSheetModal, BottomSheetModalProvider} from "@gorhom/bottom-sheet";
+import {useCallback, useMemo, useRef, useState} from "react";
+import AuthModal from "@/components/AuthModal";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Hello World.</Text>
-    </View>
-  );
+    const {top} = useSafeAreaInsets();
+    const {showActionSheetWithOptions} = useActionSheet();
+    const bottomSheetModalRef = useRef<BottomSheetModal>(null);
+
+    const snapPoints = useMemo(() => ['33%'], []);
+    const [authType, setAuthType] = useState<ModalType | null>(null);
+
+    const showModal = async (type: ModalType) => {
+        setAuthType(type);
+        await bottomSheetModalRef.current?.present();
+    };
+
+    const openLink = () => {
+        WebBrowser.openBrowserAsync('https://trollface.dk/');
+    };
+
+    const openActionSheet = async () => {
+        const options = ['View support docs', 'Contact us', 'Cancel'];
+        const cancelButtonIndex = 2;
+
+        showActionSheetWithOptions(
+            {
+                options,
+                cancelButtonIndex,
+                title: `Can't log in or sign up?`,
+            },
+            (selectedIndex: any) => {
+                switch (selectedIndex) {
+                    case 1:
+                        // Support
+                        break;
+
+                    case cancelButtonIndex:
+                    // Canceled
+                }
+            }
+        );
+    };
+
+    const renderBackdrop = useCallback(
+        (props: any) => (
+            <BottomSheetBackdrop
+                opacity={0.2}
+                appearsOnIndex={0}
+                disappearsOnIndex={-1}
+                {...props}
+                onPress={() => bottomSheetModalRef.current?.close()}
+            />
+        ),
+        []
+    );
+
+    return (
+        <BottomSheetModalProvider>
+            <View
+                style={[styles.container, {paddingTop: top + 30}]}
+            >
+                <Image
+                    source={require("@/assets/images/login/trello.png")}
+                    style={styles.image}
+                />
+                <Text style={styles.introText}>Move teamwork forward - even on the go</Text>
+
+                <View style={styles.bottomContainer}>
+                    <TouchableOpacity style={[styles.btn, {backgroundColor: "#fff"}]}
+                                      onPress={() => showModal(ModalType.Login)}>
+                        <Text style={[styles.btnText, {color: Colors.primary}]}>Log In</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.btn} onPress={() => showModal(ModalType.SignUp)}>
+                        <Text style={[styles.btnText, {color: "#fff"}]}>Sign Up</Text>
+                    </TouchableOpacity>
+                    <Text style={styles.description}>
+                        By signing up, you agree to the{' '}
+                        <Text style={styles.link} onPress={openLink}>
+                            User Notice
+                        </Text>{' '}
+                        and{' '}
+                        <Text style={styles.link} onPress={openLink}>
+                            Privacy Policy
+                        </Text>
+                        .
+                    </Text>
+
+                    <Text style={styles.link} onPress={openActionSheet}>
+                        Can't log in our sign up?
+                    </Text>
+
+                </View>
+            </View>
+            <BottomSheetModal
+                ref={bottomSheetModalRef}
+                index={0}
+                backdropComponent={renderBackdrop}
+                snapPoints={snapPoints}
+                handleComponent={null}
+                enableOverDrag={false}
+                enablePanDownToClose
+            >
+                <AuthModal authType={authType} />
+            </BottomSheetModal>
+        </BottomSheetModalProvider>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: Colors.primary,
+        alignItems: 'center',
+    },
+    image: {
+        height: 450,
+        paddingHorizontal: 40,
+        resizeMode: 'contain',
+    },
+    introText: {
+        fontWeight: '600',
+        color: 'white',
+        fontSize: 17,
+        padding: 30,
+    },
+    bottomContainer: {
+        width: '100%',
+        paddingHorizontal: 40,
+        gap: 10,
+    },
+    btn: {
+        padding: 10,
+        borderRadius: 8,
+        alignItems: 'center',
+        borderColor: '#fff',
+        borderWidth: 1,
+    },
+    btnText: {
+        fontSize: 18,
+    },
+    description: {
+        fontSize: 12,
+        textAlign: 'center',
+        color: '#fff',
+        marginHorizontal: 60,
+    },
+    link: {
+        color: '#fff',
+        fontSize: 12,
+        textAlign: 'center',
+        textDecorationLine: 'underline',
+    },
+});
